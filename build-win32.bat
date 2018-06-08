ceac2029eb2d4fe5e0a9bd5746dcdb34f15efb7c
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-06-08 16:45:20 +0300

    global fix

--- build-win32.bat
+++ build-win32.bat
@@ -46,7 +46,8 @@ signtool.exe sign /t http://timestamp.comodoca.com/authenticode /f ..\gapminder.
 call powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('Gapminder Offline-win32', 'Gapminder Offline-win32.zip'); }"
 mkdir "partial\Gapminder Offline-win32"
 call powershell.exe -nologo -noprofile -command "& {Copy-Item 'Gapminder Offline-win32\resources' -destination 'partial\Gapminder Offline-win32\resources' -recurse}"
-del /s /q ".\partial\Gapminder Offline-win32\resources\ddf--gapminder--systema_globalis"
+rem del /s /q ".\partial\Gapminder Offline-win32\resources\ddf--gapminder--systema_globalis"
+call powershell.exe -nologo -noprofile -command "& {Remove-Item -Recurse -Force 'partial\Gapminder Offline-win32\resources\ddf--gapminder--systema_globalis'}"
 cd partial
 call powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('Gapminder Offline-win32', 'Gapminder Offline-win32.zip'); }"
 cd ../..
