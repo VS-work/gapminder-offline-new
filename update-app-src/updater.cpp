f8ebf0b0abf4e347d33b429f65227476716f4e9a
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-06-11 18:39:51 +0300

    fix win updaters

--- update-app-src/updater.cpp
+++ update-app-src/updater.cpp
@@ -7,7 +7,7 @@ int main() {
 
   system("cd cache-app & robocopy . .. /XF updater-win64.exe updater-win32.exe /E & cd .. & rmdir /s /q cache-app & del .\\release-app.zip");
 
-  system("cd cache-ds & powershell.exe -nologo -noprofile -command \"& {Get-ChildItem -Filter ddf--gapminder--systema_globalis* | Rename-Item -NewName ddf--gapminder--systema_globalis}\" & robocopy ddf--gapminder--systema_globalis ..\\resources\\app\\ddf--gapminder--systema_globalis /E & cd .. & rmdir /s /q cache-ds");
+  system("cd cache-ds & powershell.exe -nologo -noprofile -command \"& {Get-ChildItem -Filter ddf--gapminder--systema_globalis* | Rename-Item -NewName ddf--gapminder--systema_globalis}\" & robocopy ddf--gapminder--systema_globalis ..\\resources\\ddf--gapminder--systema_globalis /E & cd .. & rmdir /s /q cache-ds");
 
   system("del .\\update-required");
   system("del .\\updating");
